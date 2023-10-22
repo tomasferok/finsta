eae578c531639ff565132a6eb40e5de1d64501ad
Tomas <tomaasfer11@gmail.com>
2023-10-21 23:01:43 -0300

    handle info | get all post

--- lib/finsta/posts.ex
+++ lib/finsta/posts.ex
@@ -1,6 +1,17 @@
 defmodule Finsta.Posts do
+  import Ecto.Query, warn: false
   alias Finsta.Repo
   alias Finsta.Posts.Post
+
+  def list_posts do
+    query =
+      from p in Post,
+      select: p,
+      order_by: [desc: :inserted_at],
+      preload: [:user]
+
+    Repo.all(query)
+  end
 
   def save(post_params) do
     %Post{}
--- lib/finsta_web/home_live.ex
+++ lib/finsta_web/home_live.ex
@@ -3,34 +3,81 @@ defmodule FinstaWeb.HomeLive do
 
   alias Finsta.Posts
   alias Finsta.Posts.Post
+  alias Finsta.Posts
+
+  @impl true
+  def render(%{loading: true} = assigns) do
+    ~H"""
+    Finsta is loading...
+    """
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1 class="text-2xl">Finsta</h1>
+
+    <.button type="button" phx-click={show_modal("new-post-modal")}>Create Post</.button>
+
+    <div id="feed" phx-update="stream" class="flex flex-col gap-2">
+      <div
+        :for={{dom_id, post} <- @streams.posts}
+        id={dom_id}
+        class="w-1/2 mx-auto flex flex-col gap-2 p-4 border rounded"
+      >
+        <img src={post.image_path} />
+        <p><%= post.user.email %></p>
+        
+        <p><%= post.caption %></p>
+      </div>
+    </div>
+
+    <.modal id="new-post-modal">
+      <.simple_form for={@form} phx-change="validate" phx-submit="save-post">
+        <.live_file_input upload={@uploads.image} required />
+        <.input field={@form[:caption]} type="textarea" label="Caption" required />
+        <.button type="submit" phx-disable-with="Saving .....">Create Post</.button>
+      </.simple_form>
+    </.modal>
+    """
+  end
 
   @impl true
   def render(assigns) do
     ~H"""
     <h1 class="text-2xl">Finsta</h1>
-      <.button type="button" phx-click={show_modal("new-post-modal")}>Create Post</.button>
+
+    <.button type="button" phx-click={show_modal("new-post-modal")}>Create Post</.button>
+
     <.modal id="new-post-modal">
-    <.simple_form for={@form} phx-change="validate" phx-submit="save-post">
-      <.live_file_input upload={@uploads.image} required />
-      <.input field={@form[:caption]} type="textarea" label="Caption" required />
-      <.button type="submit" phx-disable-with = "Saving .....">Create Post </.button>
-    </.simple_form>
+      <.simple_form for={@form} phx-change="validate" phx-submit="save-post">
+        <.live_file_input upload={@uploads.image} required />
+        <.input field={@form[:caption]} type="textarea" label="Caption" required />
+        <.button type="submit" phx-disable-with="Saving .....">Create Post</.button>
+      </.simple_form>
     </.modal>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    form =
-      %Post{}
-      |> Post.changeset(%{})
-      |> to_form(as: "post")
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Finsta.PubSub, "posts")
 
-    socket =
-      socket
-      |> assign(form: form)
-      |> allow_upload(:image, accept: ~w(.png .jpg), max_entries: 1)
+      form =
+        %Post{}
+        |> Post.changeset(%{})
+        |> to_form(as: "post")
+
+      socket =
+        socket
+        |> assign(form: form, loading: false)
+        |> allow_upload(:image, accept: ~w(.png .jpg), max_entries: 1)
+        |> stream(:posts, Posts.list_posts())
+
       {:ok, socket}
+    else
+      {:ok, assign(socket, loading: true)}
+    end
   end
 
   @impl true
@@ -40,20 +87,33 @@ defmodule FinstaWeb.HomeLive do
 
   def handle_event("save-post", %{"post" => post_params}, socket) do
     %{current_user: user} = socket.assigns
+
     post_params
     |> Map.put("user_id", user.id)
     |> Map.put("image_path", List.first(consume_files(socket)))
-    |> Post.save()
+    |> Posts.save()
     |> case do
-      {:ok, _post} ->
+      {:ok, post} ->
         socket =
           socket
           |> put_flash(:info, "Post created successfully")
           |> push_navigate(to: ~p"/home")
-          {:noreply, socket}
+
+        Phoenix.PubSub.broadcast(Finsta.PubSub, "posts", {:new, Map.put(post, :user, user)})
+        {:noreply, socket}
+
       {:error, _changeset} ->
-    {:noreply, socket}
+        {:noreply, socket}
+    end
   end
+
+  def handle_info({:new, post}, socket) do
+    socket =
+      socket
+      |> put_flash(:info, "#{post.user.email} just posted !")
+      |> stream_insert(:posts, post, at: 0)
+
+    {:noreply, socket}
   end
 
   defp consume_files(socket) do
@@ -63,6 +123,4 @@ defmodule FinstaWeb.HomeLive do
       {:postpone, ~p"/uploads/#{Path.basename(dest)}"}
     end)
   end
-
-
 end
